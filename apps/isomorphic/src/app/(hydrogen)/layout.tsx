612b4ace7dacbb5492c43a350b8c0247de64d98b
deannasution21 <deannasution21@gmail.com>
2025-10-18 06:36:27 +0700

    uye

--- apps/isomorphic/src/app/(hydrogen)/layout.tsx
+++ apps/isomorphic/src/app/(hydrogen)/layout.tsx
@@ -4,6 +4,7 @@ import { useIsMounted } from '@core/hooks/use-is-mounted';
 import HeliumLayout from '@/layouts/helium/helium-layout';
 import { useLayout } from '@/layouts/use-layout';
 import { LAYOUT_OPTIONS } from '@/config/enums';
+import { usePathname } from 'next/navigation';
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -16,10 +17,16 @@ export default function DefaultLayout({ children }: LayoutProps) {
 function LayoutProvider({ children }: LayoutProps) {
   const { layout } = useLayout();
   const isMounted = useIsMounted();
+  const pathname = usePathname();
 
   if (!isMounted) {
     return null;
   }
 
+  if (pathname === '/') {
+    return <>{children}</>;
+  }
+
+  // Otherwise, wrap all other routes in HeliumLayout
   return <HeliumLayout>{children}</HeliumLayout>;
 }

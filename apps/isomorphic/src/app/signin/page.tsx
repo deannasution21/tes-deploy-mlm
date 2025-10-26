bf2acb3f10252e34260fba8cfce332256f394820
deannasution21 <deannasution21@gmail.com>
2025-10-26 23:19:44 +0700

    update 2

--- apps/isomorphic/src/app/signin/page.tsx
+++ apps/isomorphic/src/app/signin/page.tsx
@@ -1,9 +1,5 @@
 import SignInForm from '@/app/signin/sign-in-form';
-import AuthWrapperOne from '@/app/shared/auth-layout/auth-wrapper-one';
-import Image from 'next/image';
-import UnderlineShape from '@core/components/shape/underline';
 import { metaObject } from '@/config/site.config';
-import loginImg from '@public/assets/img/image (1).png';
 
 import '@public/assets/css/animate.css';
 import '@public/assets/css/main.css';
@@ -12,37 +8,6 @@ import AuthWrapperFour from '../shared/auth-layout/auth-wrapper-four';
 export const metadata = {
   ...metaObject('Sign In'),
 };
-
-export function SignIn1() {
-  return (
-    <AuthWrapperOne
-      title={
-        <h2 className="wow fadeInUp" data-wow-delay=".2s">
-          Selamat Datang Orang Hebat!
-        </h2>
-      }
-      description="Silakan masuk untuk melanjutkan ke dashboard Anda."
-      bannerTitle="Kesuksesan tidak datang kepada mereka yang menunggu, tetapi kepada mereka yang berani mengambil langkah pertama."
-      bannerDescription="— Eleanor Roosevelt"
-      isSocialLoginActive={false}
-      pageImage={
-        <div className="relative mx-auto aspect-[4/3.37] w-[500px] xl:w-[620px] 2xl:w-[820px]">
-          <Image
-            src={loginImg}
-            alt="Sign Up Thumbnail"
-            fill
-            priority
-            sizes="(max-width: 768px) 100vw"
-            className="object-cover"
-          />
-        </div>
-      }
-      role="user"
-    >
-      <SignInForm role="user" />
-    </AuthWrapperOne>
-  );
-}
 
 export default function SignIn() {
   return (
